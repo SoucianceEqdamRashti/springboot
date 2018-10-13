da87829e4088d8fa6d3e03bdbf9598e2ea481630
Souciance <souciance.eqdam.rashti@gmail.com>
2018-10-13 18:43:46 +0200

    Main spring boot class

---
+++ sendemail/src/main/java/com/souciance/boot/sendemail/SendEmailApplication.java
@@ -0,0 +1,14 @@
+package com.souciance.boot.sendemail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendEmailApplication {
+    public static void main(String[] args) {
+
+        //start the spring boot app
+		SpringApplication.run(SendEmailApplication.class, args);
+
+	}
+}
